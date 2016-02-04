dfb846b74e3e7e54b78180bf47a495689d837bd8
Kirby Bryant <akcbryant@gmail.com>
2016-02-04 15:59:27 -0800

    Final Refactor

--- src/kata3/core.clj
+++ src/kata3/core.clj
@@ -4,10 +4,7 @@
   (filter #(zero? (mod n %)) (range 1 (inc n))))
 
 (defn prime? [n]
-  (cond (= n 1) false
-        (and (< n 4) (>= n 2)) true
-        (even? n) false
-        :else (= 2 (count (factors n)))))
+  (= 2 (count (factors n))))
 
 (defn prime-factors [n]
   (filter prime? (factors n)))
